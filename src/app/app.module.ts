5e7aa4e8a77c057d3e8b0e6178c06bd643a3ca15
marcin.matula <marqnpl@gmail.com>
2017-03-01 14:02:47 +0100

    simple routing implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import  {RouterModule, Routes} from '@angular/router'
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {RouterModule, Routes} from "@angular/router";
+import {AppComponent} from "./app.component";
+import {LoginComponent} from "./login/login.component";
+import {AngularFireModule, AuthProviders, AuthMethods} from "angularfire2";
 import {AuthService} from "./auth.service";
 
-/*
-const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: '**', component: PageNotFoundComponent }
+const routes:Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'startpage', component: LoginComponent},
+  {path: 'game', component: LoginComponent}
 ];
-*/
+
 
 // Must export the config
 export const firebaseConfig = {
@@ -38,9 +39,11 @@ const myFirebaseAuthConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig),
+    RouterModule.forRoot(routes)
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
